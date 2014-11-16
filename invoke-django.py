987505ad8704d0c4b17bae7c594360cd64fdd8eb
Jon Gjengset <jon@thesquareplanet.com>
2014-11-16 14:43:17 -0500

    Split out symdjango

--- invoke-django.py
+++ invoke-django.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
 
+import os
+from symex import symdjango
+
 # name of app being mocked
 app = "zoobar"
 
@@ -15,105 +18,6 @@ appviews = {
         "": {}
 }
 
-import sys
-import os
-
-# search for modules inside application under test
-sys.path.append(os.path.dirname(os.path.abspath(__file__)) + '/app')
-
-# use our Django (currently irrelevant)
-sys.path.insert(1, os.path.dirname(os.path.abspath(__file__)) + '/../django-concolic')
-
-# patch Django where needed
-from unittest.mock import patch
-
-# Dynamic imports
-import importlib
-
-# Various items needed to invoke Django directly
-from django.core.urlresolvers import ResolverMatch, Resolver404, NoReverseMatch
-from django.core.handlers.wsgi import WSGIRequest
-from django.core.servers.basehttp import get_internal_wsgi_application
-from django.conf import settings
-from django.test.client import FakePayload, RequestFactory
-
-# Make sure Django reads the correct settings
-os.environ.update({
-    "DJANGO_SETTINGS_MODULE": "app." + app + ".settings"
-})
-
-class SymURL():
-    def __init__(self, mod, v):
-        self.mod = mod
-        self.view = v
-
-    @property
-    def callback(self):
-        global appviews
-        return appviews[self.mod][self.view]
-
-    def resolve(self, path):
-        if self.callback(path):
-            print("%s matches against %s.%s" % (path, self.mod, self.view))
-            kwargs = {
-                    # named groups in url
-                    }
-
-            if kwargs:
-                args = ()
-            else:
-                args = [] # unnamed args in url
-
-            kwargs.update({}) # extra args passed to view from urls.py
-            views = importlib.import_module(self.mod + '.views')
-            return ResolverMatch(getattr(views, self.view), args, kwargs, self.view)
-
-class SymResolver():
-    def __init__(self, regex, conf):
-        self.mod = ""
-
-    def setMod(self, mod):
-        self.mod = mod
-
-    def resolve(self, path):
-        print("resolving path '%s'" % path)
-        for m in appviews:
-            for v in appviews[m]:
-                s = SymURL(m, v)
-                r = s.resolve(path)
-                if r is not None:
-                    return r
-
-        raise Resolver404({'path': path})
-
-    def _reverse_with_prefix(self, v, _prefix, *args, **kwargs):
-        return "/"
-
-    @property
-    def namespace_dict(self):
-        global reverseDict
-        return reverseDict
-
-    @property
-    def app_dict(self):
-        return {}
-
-reverseDict = {}
-for m in appviews:
-    s = SymResolver("", None)
-    s.setMod(m)
-    reverseDict[m] = ("", s)
-
-class SymRequestFactory(RequestFactory):
-    def __init__(self, start_response, **defaults):
-        self.start_response = start_response
-        RequestFactory.__init__(self, *defaults)
-
-    def request(self, **request):
-        handler = get_internal_wsgi_application()
-        with patch('django.core.urlresolvers.RegexURLResolver', new=SymResolver) as urlmock:
-            return handler(self._base_environ(**request), start_response)
-
 def start_response(status, response_headers):
     print("\n%18s: %s" % ("RESULT", status))
     print("%18s: %s" % ("------", "--------------"))
@@ -121,8 +25,9 @@ def start_response(status, response_headers):
         print("%18s: %s" % (h, v))
     print("")
 
+d = symdjango.SymDjango(app, os.path.abspath(os.path.dirname(__file__) + '/app'), appviews)
 f = open('result.html', 'wb')
-req = SymRequestFactory(start_response)
+req = d.new(start_response)
 body = req.get('/accounts/login/')
 for c in body:
     f.write(c)
---
+++ symex/symdjango.py
@@ -0,0 +1,106 @@
+#!/usr/bin/env python
+
+import sys
+import os
+
+# patch Django where needed
+from unittest.mock import patch
+
+# Dynamic imports
+import importlib
+
+# use our Django (currently irrelevant)
+ourdjango = os.path.dirname(os.path.abspath(__file__)) + '/../django-concolic'
+if ourdjango not in sys.path:
+    sys.path.insert(1, ourdjango)
+
+from django.test.client import RequestFactory
+
+# It's only safe to use SymDjango as a singleton!
+class SymDjango():
+    def __init__(self, app, path, viewmap):
+        self.app = app
+        self.path = path
+        self.viewmap = viewmap
+
+        # search for modules inside application under test
+        sys.path.append(path)
+
+        # Make sure Django reads the correct settings
+        os.environ.update({
+            "DJANGO_SETTINGS_MODULE": "app." + app + ".settings"
+        })
+
+    def new(self, response_handler):
+        return SymRequestFactory(self, response_handler)
+
+class SymRequestFactory(RequestFactory):
+    def __init__(self, symdjango, start_response, **defaults):
+        self.symdjango = symdjango
+        self.start_response = start_response
+        RequestFactory.__init__(self, *defaults)
+
+    def request(self, **request):
+        from django.core.servers.basehttp import get_internal_wsgi_application
+        handler = get_internal_wsgi_application()
+        with patch('django.core.urlresolvers.RegexURLResolver', new=SymResolver) as urlmock:
+            urlmock.symdjango = self.symdjango
+            return handler(self._base_environ(**request), self.start_response)
+
+class SymResolver():
+    symdjango = None
+
+    def __init__(self, regex, conf):
+        self.reverseDict = {}
+        for m in SymResolver.symdjango.viewmap:
+            self.reverseDict[m] = ("", self)
+
+    def resolve(self, path):
+        from django.core.urlresolvers import Resolver404
+        print("resolving path '%s'" % path)
+        for m in SymResolver.symdjango.viewmap:
+            for v in SymResolver.symdjango.viewmap[m]:
+                s = SymURL(SymResolver.symdjango, m, v)
+                r = s.resolve(path)
+                if r is not None:
+                    return r
+
+        raise Resolver404({'path': path})
+
+    def _reverse_with_prefix(self, v, _prefix, *args, **kwargs):
+        return "/"
+
+    @property
+    def namespace_dict(self):
+        return self.reverseDict
+
+    @property
+    def app_dict(self):
+        return {}
+
+class SymURL():
+    def __init__(self, symdjango, mod, v):
+        self.symdjango = symdjango
+        self.mod = mod
+        self.view = v
+
+    @property
+    def callback(self):
+        return self.symdjango.viewmap[self.mod][self.view]
+
+    def resolve(self, path):
+        from django.core.urlresolvers import ResolverMatch
+        if self.callback(path):
+            print("%s matches against %s.%s" % (path, self.mod, self.view))
+            kwargs = {
+                    # named groups in url
+                    }
+
+            if kwargs:
+                args = ()
+            else:
+                args = [] # unnamed args in url
+
+            kwargs.update({}) # extra args passed to view from urls.py
+            views = importlib.import_module(self.mod + '.views')
+            return ResolverMatch(getattr(views, self.view), args, kwargs, self.view)
