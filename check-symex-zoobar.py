92db342764082f5a096234aa395408a96b4b83e1
Jon Gjengset <jon@thesquareplanet.com>
2014-11-16 16:10:06 -0500

    Use symdjango in check-symex-zoobar

--- check-symex-zoobar.py
+++ check-symex-zoobar.py
@@ -1,7 +1,8 @@
-#!/usr/bin/python
+#!/usr/bin/env python2
 
 verbose = True
 
+import os
 import symex.fuzzy as fuzzy
 import __builtin__
 import inspect
@@ -10,48 +11,53 @@ import symex.rewriter as rewriter
 
 importwrapper.rewrite_imports(rewriter.rewriter)
 
-import symex.symflask
+from symex.symdjango import SymDjango
 import symex.symsql
 import symex.symeval
-import zoobar
+
+app = "zoobar"
+appviews = {
+        "zapp": {
+            "index": (lambda p: p == "/")
+        },
+        "zlogio": {
+            "login": (lambda p: p == "/accounts/login/"),
+            "logout": (lambda p: p == "/accounts/logout/"),
+        },
+        "": {}
+}
 
 def startresp(status, headers):
   if verbose:
-    print 'startresp', status, headers
+    print('startresp', status, headers)
+
+d = SymDjango(app, os.path.abspath(os.path.dirname(__file__) + '/app'), appviews)
+
+# Only safe to load now that it's been patched and added to import path
+import zoobar
 
 def report_balance_mismatch():
-  print "WARNING: Balance mismatch detected"
+  print("WARNING: Balance mismatch detected")
 
 def report_zoobar_theft():
-  print "WARNING: Zoobar theft detected"
+  print("WARNING: Zoobar theft detected")
 
-def adduser(pdb, username, token):
-  u = zoobar.zoodb.Person()
-  u.username = username
-  u.token = token
-  pdb.add(u)
+def adduser(username):
+  from django.contrib.auth.models import User
+  u = User.objects.create_user(username, '', 'password')
+  u.save()
 
 def test_stuff():
-  pdb = zoobar.zoodb.person_setup()
-  pdb.query(zoobar.zoodb.Person).delete()
-  adduser(pdb, 'alice', 'atok')
-  adduser(pdb, 'bob', 'btok')
-  balance1 = sum([p.zoobars for p in pdb.query(zoobar.zoodb.Person).all()])
-  pdb.commit()
+  req = d.new(startresp)
 
-  tdb = zoobar.zoodb.transfer_setup()
-  tdb.query(zoobar.zoodb.Transfer).delete()
-  tdb.commit()
+  from django.contrib.auth.models import User
+  User.objects.all().delete()
+  adduser('alice')
+  adduser('bob')
+  balance1 = sum([u.person.zoobars for u in User.objects.all()])
 
-  environ = {}
-  environ['wsgi.url_scheme'] = 'http'
-  environ['wsgi.input'] = 'xxx'
-  environ['SERVER_NAME'] = 'zoobar'
-  environ['SERVER_PORT'] = '80'
-  environ['SCRIPT_NAME'] = 'script'
-  environ['QUERY_STRING'] = 'query'
-  environ['HTTP_REFERER'] = fuzzy.mk_str('referrer')
-  environ['HTTP_COOKIE'] = fuzzy.mk_str('cookie')
+  from zapp.models import Transfer
+  Transfer.objects.all().delete()
 
   ## In two cases, we over-restrict the inputs in order to reduce the
   ## number of paths that "make check" explores, so that it finishes
@@ -59,21 +65,20 @@ def test_stuff():
   ## concolic values for both REQUEST_METHOD and PATH_INFO, but then
   ## zoobar generates around 2000 distinct paths, and that takes many
   ## minutes to check.
-
-  # environ['REQUEST_METHOD'] = fuzzy.mk_str('method')
-  # environ['PATH_INFO'] = fuzzy.mk_str('path')
-  environ['REQUEST_METHOD'] = 'GET'
-  environ['PATH_INFO'] = 'trans' + fuzzy.mk_str('path')
-
-  if environ['PATH_INFO'].startswith('//'):
+  path = '/trans' + fuzzy.mk_str('path')
+  if path.startswith('//'):
     ## Don't bother trying to construct paths with lots of slashes;
     ## otherwise, the lstrip() code generates lots of paths..
     return
 
-  resp = zoobar.app(environ, startresp)
+  resp = req.get(path
+      , HTTP_COOKIE  = fuzzy.mk_str('cookie') # this probably won't work for Django
+      , HTTP_REFERER = fuzzy.mk_str('referrer') # why is this fuzzed?
+      )
+
   if verbose:
     for x in resp:
-      print x
+      print(x)
 
   ## Exercise 6: your code here.
 
@@ -84,4 +89,3 @@ def test_stuff():
   ## When detected, call report_zoobar_theft()
 
 fuzzy.concolic_test(test_stuff, maxiter=2000, verbose=1)
-
