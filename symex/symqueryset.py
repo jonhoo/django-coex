63ad8047d928d2d244c1ef0fb5eb5b9887197bda
hyperkerfuffle <hyperkerfuffle>
2014-12-06 21:20:21 +0000

    Fix some indentation conflicts

--- symex/symqueryset.py
+++ symex/symqueryset.py
@@ -28,20 +28,20 @@ class SymQuerySet(QuerySet, SymMixin):
   operators = ['lte', 'gte', 'gt', 'lt', 'exact'];
 
   # TODO: merge with newget in symdjango
-  def get(self, *args, **kwargs):
-    if len(kwargs) == 1:
-      key = kwargs.keys()[0]
-        value = kwargs[value]
+  #def get(self, *args, **kwargs):
+    #if len(kwargs) == 1:
+      #key = kwargs.keys()[0]
+        #value = kwargs[value]
 
-        if '_' not in key and isinstance(value, concolic_str):
-          if key == 'pk':
-            key = self.model._meta.pk.name
-            kwargs[key] = kwargs['pk']
-            del kwargs['pk']
+        #if '_' not in key and isinstance(value, concolic_str):
+          #if key == 'pk':
+            #key = self.model._meta.pk.name
+            #kwargs[key] = kwargs['pk']
+            #del kwargs['pk']
 
-          result = super(SymQuerySet, self).get(args, kwargs)
+          #result = super(SymQuerySet, self).get(args, kwargs)
 
-      return super(SymQuerySet, self).get(args, kwargs)
+      #return super(SymQuerySet, self).get(args, kwargs)
 
   def filter(self, *args, **kwargs):
     (op, value, mutations) = self._mutate(*args, **kwargs)
@@ -52,10 +52,10 @@ class SymQuerySet(QuerySet, SymMixin):
 
   def _apply_filter(self, *args, **kwargs):
     from django.core.exceptions import FieldError
-      try:
-        return super(SymQuerySet, self).filter(*args, **kwargs)
-      except FieldError: 
-        return None
+    try:
+      return super(SymQuerySet, self).filter(*args, **kwargs)
+    except FieldError: 
+      return None
 
   #
   # Based on ConSMutate: SQL mutants for guiding concolic testing of database 
@@ -84,38 +84,38 @@ class SymQuerySet(QuerySet, SymMixin):
   #
   def _mutate(self, *args, **kwargs):
     mutations = {} 
-      for arg in kwargs:
-        lookups, parts, reffed_aggregate = self.query.solve_lookup_type(arg)
+    for arg in kwargs:
+      lookups, parts, reffed_aggregate = self.query.solve_lookup_type(arg)
 
-        if len(lookups) != 1:
-          continue
+      if len(lookups) != 1:
+        continue
 
-        mutated_filters = {}
-        operator = lookups[0]
-        filter_column = '_'.join(parts)
-        filter_value = kwargs[arg]
+      mutated_filters = {}
+      operator = lookups[0]
+      filter_column = '_'.join(parts)
+      filter_value = kwargs[arg]
 
-        mutate_operators = [op for op in self.operators if op != operator]
-        for op in mutate_operators:
-          mutated_filters[op] = {filter_column + '__' + op: filter_value}
+      mutate_operators = [op for op in self.operators if op != operator]
+      for op in mutate_operators:
+        mutated_filters[op] = {filter_column + '__' + op: filter_value}
 
-        # TODO: currently only handles filters with single column queries
-        # e.g. username='alice'. Ideally, this would handle filters over
-        # multiple columns e.g. find the transfers of more than 10 zoobars 
-        # to alice recipient='alice' && zoobars > 10
-        #break
-        return (op, filter_value, self._create_mutated_querysets(mutated_filters, *args))
+      # TODO: currently only handles filters with single column queries
+      # e.g. username='alice'. Ideally, this would handle filters over
+      # multiple columns e.g. find the transfers of more than 10 zoobars 
+      # to alice recipient='alice' && zoobars > 10
+      #break
+      return (op, filter_value, self._create_mutated_querysets(mutated_filters, *args))
 
-        #mutations.append(mutation_set)
+      #mutations.append(mutation_set)
 
-      return mutations
+    return mutations
 
   def _create_mutated_querysets(self, mutated_filters, *args):
     mutations = {}
     for op in mutated_filters:
       filter_kv = mutated_filters[op]
-        mutated_queryset = self._apply_filter(*args, **filter_kv)
-        mutations[op] = mutated_queryset
+      mutated_queryset = self._apply_filter(*args, **filter_kv)
+      mutations[op] = mutated_queryset
     return mutations
 
   def _remove_dead_mutations(self, original_queryset, mutations):
@@ -123,8 +123,8 @@ class SymQuerySet(QuerySet, SymMixin):
     items = list(six.moves.map(repr, original_queryset))
     for op in mutations:
       mutation = mutations[op]
-        if self._is_equal(items, mutation):
-          unique_mutations[op] = mutation
+      if self._is_equal(items, mutation):
+        unique_mutations[op] = mutation
     return unique_mutations
 
   def _is_equal(self, values, other_queryset):
