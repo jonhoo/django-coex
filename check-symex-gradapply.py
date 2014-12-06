b7cffef5a8ff0c319daafe2773d471f4b5a6596c
Jeevana <jinala@mit.edu>
2014-12-05 23:21:53 -0500

    Some more changes to gradapply checking script

--- check-symex-gradapply.py
+++ check-symex-gradapply.py
@@ -38,41 +38,47 @@ appviews = {
 
 d = SymDjango(settings, os.path.abspath(os.path.dirname(__file__) + '../gradapply'), appviews)
 
+from django.test import TestCase
+from django.test.utils import setup_test_environment
 # Only safe to load now that it's been patched and added to import path
 import apply
 
 # TODO(jon): This currently only test single-request actions
-class concolicTest(TestCase):
-fixtures = ['testdb/login_user', 'testdb/login_conf', 'testdb/review_reader']
-def test_stuff():
-  method = fuzzy.mk_str('method')
-  if not method == 'get' and not method == 'post':
+class ConcolicTestCase(TestCase):
+  fixtures = ['../gradapply/apply/fixtures/testdb/login_user', '../gradapply/apply/fixtures/testdb/login_conf', '../gradapply/apply/fixtures/testdb/review_reader']
+  
+  def runTest(self):
     return
+   
+  def test_stuff(self):
+    method = fuzzy.mk_str('method')
+    if not method == 'get' and not method == 'post':
+      return
 
-  req = d.new()
+    req = d.new()
  
-  ## In two cases, we over-restrict the inputs in order to reduce the
-  ## number of paths that "make check" explores, so that it finishes
-  ## in a reasonable amount of time.  You could pass unconstrained
-  ## concolic values for both REQUEST_METHOD and PATH_INFO, but then
-  ## zoobar generates around 2000 distinct paths, and that takes many
-  ## minutes to check.
-  path = 'apply/'
-  if path[0] == '/':
-    return
+    ## In two cases, we over-restrict the inputs in order to reduce the
+    ## number of paths that "make check" explores, so that it finishes
+    ## in a reasonable amount of time.  You could pass unconstrained
+    ## concolic values for both REQUEST_METHOD and PATH_INFO, but then
+    ## zoobar generates around 2000 distinct paths, and that takes many
+    ## minutes to check.
+    path = 'apply/'
+    if path[0] == '/':
+      return
 
-  data = {}
-  '''if method == 'post':
-    if path == 'transfer/':
-      data = post_data(
-        zoobars = fuzzy.mk_int('transfer.zoobars'),
-        recipient = fuzzy.mk_str('transfer.recipient')
-      )'''
+    data = {}
+    '''if method == 'post':
+      if path == 'transfer/':
+        data = post_data(
+          zoobars = fuzzy.mk_int('transfer.zoobars'),
+          recipient = fuzzy.mk_str('transfer.recipient')
+        )'''
 
-  logged_in = False
-  ok = True
-  user = fuzzy.mk_str('user')
-  if user == 'eval-kaashoek' or user == 'apply-abarry':
+    logged_in = False
+    ok = True
+    user = fuzzy.mk_str('user')
+    if user == 'eval-kaashoek' or user == 'apply-abarry':
       if verbose > 0:
         print('==> accessing %s as %s' % (path, user))
 
@@ -82,38 +88,38 @@ def test_stuff():
           ok = req.login(username='apply-abarry', password='yyy')
 
       logged_in = True
-  else:
+    else:
       if verbose > 0:
         print('==> accessing %s anonymously' % path)
-  if (not ok):
-    print(" Loggin unsuccessful")
-  response = None
-  if method == 'get':
-    response = req.get(path)
-  elif method == 'post':
-    response = req.post(path, data=data)
+    if (not ok):
+      print(" Loggin unsuccessful")
+    response = None
+    if method == 'get':
+      response = req.get(path)
+    elif method == 'post':
+      response = req.post(path, data=data)
 
-  if verbose == 1 and response.status_code == 404:
-    print(" -> 404 not found...")
-  elif verbose == 1:
-    print(' -> %d %s' % (response.status_code, response.reason_phrase))
-  elif verbose > 1:
-    print(' -> %d %s\n -> %s' % (
-      response.status_code,
-      response.reason_phrase,
-      response.items())
-      )
+    if verbose == 1 and response.status_code == 404:
+      print(" -> 404 not found...")
+    elif verbose == 1:
+      print(' -> %d %s' % (response.status_code, response.reason_phrase))
+    elif verbose > 1:
+      print(' -> %d %s\n -> %s' % (
+        response.status_code,
+        response.reason_phrase,
+        response.items())
+        )
 
-  if verbose > 2 or response.status_code == 500:
-    print(80 * "-")
-    print(re.sub("^", "\t", response.content))
-    print(80 * "-")
+    if verbose > 2 or response.status_code == 500:
+      print(80 * "-")
+      print(re.sub("^", "\t", response.content))
+      print(80 * "-")
 
-  if logged_in and path == "transfer/":
+    if logged_in and path == "transfer/":
       if "Log out" in response.content:
-          print(" -> login works. that's nice.")
+        print(" -> login works. that's nice.")
       else:
-          print(" -> login doesn't work :(")
+        print(" -> login doesn't work :(")
 
       if method == "post":
         if "warning" in response.content:
@@ -126,19 +132,23 @@ def test_stuff():
           print(re.sub("^", "\t", response.content))
           print(80 * "-")
 
-  '''if User.objects.all().count() == 2:
-    balance2 = sum([u.person.zoobars for u in User.objects.all()])
-    if balance1 != balance2:
-      report_balance_mismatch()
+    '''if User.objects.all().count() == 2:
+      balance2 = sum([u.person.zoobars for u in User.objects.all()])
+      if balance1 != balance2:
+        report_balance_mismatch()
 
-  utransfers = [t.sender.user.username for t in Transfer.objects.all()]
-  for p in User.objects.all():
-    if p.username not in utransfers:
-      if p.person.zoobars < 10:
-        report_zoobar_theft()
-        # technically, this check could be fooled if an attacker could insert
-        # rows into the transfer db. Instead, we should keep a log of all
-        # requests, and which user the request was issued as, but this seems
-        # outside the scope of the exercise?'''
+    utransfers = [t.sender.user.username for t in Transfer.objects.all()]
+    for p in User.objects.all():
+      if p.username not in utransfers:
+        if p.person.zoobars < 10:
+          report_zoobar_theft()
+          # technically, this check could be fooled if an attacker could insert
+          # rows into the transfer db. Instead, we should keep a log of all
+          # requests, and which user the request was issued as, but this seems
+          # outside the scope of the exercise?'''
+setup_test_environment()
 
-fuzzy.concolic_test(test_stuff, maxiter=2000, verbose=verbose)
+from django.test.simple import DjangoTestSuiteRunner
+DjangoTestSuiteRunner().setup_databases()
+test_case = ConcolicTestCase()
+fuzzy.concolic_test(test_case.test_stuff, maxiter=2000, verbose=verbose)
